5d4e6d4bb91487882abfb4155421a17b39f33dc8
Roger Pence <roger.pence@gmail.com>
2022-03-03 22:47:10 -0600

    filter working but not showing filter in text box

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 using ASNA.IBMiAccess;
 using mvc_with_avr.Models;
@@ -7,26 +8,38 @@ namespace mvc_with_avr.Controllers
 {
     public class CustomerController : Controller
     {
+        public ActionResult ClearSearch()
+        {
+            Session.Remove("whereClause");
+            return RedirectToAction("Index", new { page = 1 });
+        }
+
         public ActionResult Index(int? page)
         {
             int pageNumber = (page.HasValue) ? page.Value : 1;
 
             PagedDataManager pdm = new PagedDataManager();
+            if (Session["whereClause"] != null) 
+            {
+                pdm.WhereClause = Session["whereClause"].ToString();
+            }
+
             List<CustomerPageModel> cpm =
                    pdm.GetPageData(PageNumber: pageNumber);
 
             ViewBag.MorePages = pdm.MorePagesToShow;
-            ViewBag.NextPage = ++pageNumber;
-            ViewBag.PrevPage = pageNumber - 2;
+            ViewBag.NextPage = pageNumber + 1;
+            ViewBag.PrevPage = pageNumber - 1;
             return View(cpm);
         }
 
         [HttpPost]
         public ActionResult Search(string search)
         {
-            string formSearch = search;
+            string whereClause = String.Format("WHERE LOWER(CONCAT(cmcustno,trim(cmname))) LIKE '%{0}%'", search.ToLower().Trim());
+            Session["whereClause"] = whereClause;
 
-            return View();
+            return RedirectToAction("Index", new { page = 1 }); 
         }
     }
 }
